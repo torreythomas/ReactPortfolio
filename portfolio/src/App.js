8e04e1e3682e95fde3d4e7f72bcf3141c4636a48
Torreythms <torreythms@gmail.com>
2020-06-16 23:35:22 -0400

    created the website landing page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,16 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="social-media-links">
+        <i class="fab top-bar fa-linkedin"></i> 
+        <i class="fab top-bar  fa-github-square"></i>
+        <i class="fab top-bar fa-medium"></i>
+        </div>
       <div className="container">
       <div id="home">
-<h1 className="torrey"> Torrey </h1>
-<h1 className="thomas"> Thomas </h1>
+<h1 className="torrey"><span className="closing-tag"><i class="fas fa-chevron-left"></i></span>Torrey </h1>
+    <h1 className="thomas "> Thomas<span className="closing-tag">/<i class="fas fa-chevron-right"></i></span> <p className="web-dev"> id="Web Developer"<span class="blinking-cursor">|</span> </p> </h1>
 </div>
 <div className="navigation-bar">
  <ul>
-   <li className="nav-item item-1"> Hey</li>
-   <li className="nav-item item-2"> Hey</li>
-   <li className="nav-item item-3"> Hey</li>
+   <li className="nav-item item-1"> Me </li>
+   <li className="nav-item item-2"> What I've Made</li>
+   <li className="nav-item item-3"> Let's Talk</li>
    </ul>
 </div>
 </div>
