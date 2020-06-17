4aac0c4d1c12d8eb426a0383c06a99489a41f25d
Torreythms <torreythms@gmail.com>
2020-06-16 22:46:12 -0400

    basic landing page for my new portfolio

---
+++ portfolio/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+      <div id="home">
+<h1 className="torrey"> Torrey </h1>
+<h1 className="thomas"> Thomas </h1>
+</div>
+<div className="navigation-bar">
+ <ul>
+   <li className="nav-item item-1"> Hey</li>
+   <li className="nav-item item-2"> Hey</li>
+   <li className="nav-item item-3"> Hey</li>
+   </ul>
+</div>
+</div>
+    </div>
+  );
+}
+
+export default App;
