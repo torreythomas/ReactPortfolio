4854647c684ff603d8bf079688a2177499a46bc0
Torreythms <torreythms@gmail.com>
2020-06-16 23:54:58 -0400

    mvp landing page completed

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.css';
+import 'tachyons';
 
 function App() {
   return (
     <div className="App">
       <div className="social-media-links">
-        <i class="fab top-bar fa-linkedin"></i> 
-        <i class="fab top-bar  fa-github-square"></i>
-        <i class="fab top-bar fa-medium"></i>
+        <i class="fab grow top-bar  fa-linkedin"></i> 
+        <i class="fab grow top-bar  fa-github-square"></i>
+        <i class="fab grow top-bar fa-medium"></i>
         </div>
+        <div className="codenumbers">
+  <p>1</p><p>2</p><p>3</p><p>4</p><p>5</p><p>6</p><p>7</p><p>8</p><p>9</p><p>10</p><p>11</p><p>12</p><p>13</p><p>14</p><p>15</p><p>16</p><p>17</p><p>18</p><p>19</p><p>20</p> <p>21</p><p>22</p><p>23</p><p>24</p><p>25</p>
+</div>
       <div className="container">
       <div id="home">
-<h1 className="torrey"><span className="closing-tag"><i class="fas fa-chevron-left"></i></span>Torrey </h1>
+<h1 className="torrey "><span className="closing-tag"><i class="fas fa-chevron-left"></i></span>Torrey </h1>
     <h1 className="thomas "> Thomas<span className="closing-tag">/<i class="fas fa-chevron-right"></i></span> <p className="web-dev"> id="Web Developer"<span class="blinking-cursor">|</span> </p> </h1>
 </div>
 <div className="navigation-bar">
